fa5b4fd46ab6bd473ae0f17a8317f3356ee51b2b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2019-03-05 18:04:53 +0000

    Load aws-node assets

--- pkg/addons/default/aws_node.go
+++ pkg/addons/default/aws_node.go
@@ -1,5 +1,36 @@
 package defaultaddons
 
+import (
+	"github.com/kris-nova/logger"
+	"github.com/pkg/errors"
+
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
 const (
 	AWSNode = "aws-node"
 )
+
+func UpdateAWSNode(clientSet *kubernetes.Clientset, dryRun bool) error {
+	_, err := clientSet.Apps().DaemonSets(metav1.NamespaceSystem).Get(AWSNode, metav1.GetOptions{})
+	if err != nil {
+		if apierrs.IsNotFound(err) {
+			logger.Warning("%q was not found", AWSNode)
+			return nil
+		}
+		return errors.Wrapf(err, "getting %s", AWSNode)
+	}
+
+	// if DaemonSets is present, go through our list of assets
+	list, err := LoadAsset(AWSNode, "yaml")
+	if err != nil {
+		return err
+	}
+
+	for _, obj := range list.Items {
+		logger.Debug("%#v", obj.Object.GetObjectKind())
+	}
+	return nil
+}
